ebafe7f3b9a647e0fb531a847efb894fc750b6be
Vedanshkatara <125366269+Vedanshkatara@users.noreply.github.com>
2025-06-12 11:44:52 +0530

    Core-Java
    Completed Todays Assignment

---
+++ Day 1 Programming elements/Level 1/profitcal.java
@@ -0,0 +1,13 @@
+public class profitcal {
+    public static void main(String[] args){
+        int cp=129;
+        int sp=191;
+        int profit=sp-cp;
+        double profitpercent=(profit*100)/cp;
+        System.out.println("The cost price is " + cp);
+        System.out.println("The selling price is " + sp);   
+        System.out.println("The profit is " + profit);
+        System.out.println("The profit percent is " + profitpercent + "%");
+    }
+    
+}
